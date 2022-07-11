0621e87a20a2607e952f3ea8d99d6ee245aaa35f
김수아 <106250150+susuakim@users.noreply.github.com>
2022-07-11 16:15:08 +0900

    Add files via upload

---
+++ ex06_논리연산자.java
@@ -0,0 +1,19 @@
+package 기타연산자;
+
+public class ex06_논리연산자 {
+
+	public static void main(String[] args) {
+//		not !
+//		and &&
+//		or ||
+
+		System.out.println(true || false);
+		
+	}
+
+}
+//
+//f t false
+////f t false
+//f f false
+//f f false
