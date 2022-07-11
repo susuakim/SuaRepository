bbc6088fa01ec94fdb243e49490f07331049759a
김수아 <106250150+susuakim@users.noreply.github.com>
2022-07-11 16:14:50 +0900

    Add files via upload

---
+++ ex04_증감연산자.java
@@ -0,0 +1,26 @@
+package 기타연산자;
+
+public class ex04_증감연산자 {
+
+	public static void main(String[] args) {
+		int a =5;
+		int b =6;
+		
+		System.out.println(++a);
+		System.out.println(a++);
+		System.out.println(a);
+		
+		
+	}
+
+}
+
+o = 3
+3
+4
+5
+5
+5
+4
+3
+3
