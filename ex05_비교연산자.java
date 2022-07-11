57aae4e74b7dca1f0bf61898ee6d24801d9ca3a1
김수아 <106250150+susuakim@users.noreply.github.com>
2022-07-11 16:15:01 +0900

    Add files via upload

---
+++ ex05_비교연산자.java
@@ -0,0 +1,14 @@
+package 기타연산자;
+
+public class ex05_비교연산자 {
+
+	public static void main(String[] args) {
+		
+		int a = 3;
+		int b = 10;
+		
+		System.out.println(a>b);
+		System.out.println(a<b);
+	}
+
+}
