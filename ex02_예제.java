1b7ecbd02e889a60beafed1a3dce54d912776ea1
김수아 <106250150+susuakim@users.noreply.github.com>
2022-07-13 09:02:16 +0900

    Add files via upload

--- ex02_예제.java
+++ ex02_예제.java
@@ -1,4 +1,4 @@
-package 산술연산자;
+package 반복문while;
 
 import java.util.Scanner;
 
@@ -6,16 +6,38 @@ public class ex02_예제 {
 
 	public static void main(String[] args) {
 		
-		System.out.print("숫자입력: ");
-
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
 		
-		if(num>=100) {
-			System.out.println(num/100*100);
+		System.out.print("현재몸무게a : ");
+		int a = sc.nextInt();
+		
+		System.out.print("목표몸무게b : ");
+		int b = sc.nextInt();
+		
+		int sum = 0;
+		
+		int c = 1;
+//		while(true) {
+//			System.out.print(c++ + "주차 감량 몸무게 : ");
+//			int d = sc.nextInt();
+//		
+//			sum += d;
+//			
+//			if(sum > (a-b)) {
+//				System.out.println(a - sum + "kg 달성!!");
+//				break;
+//			} 
+			
+			while(sum < (a-b)) {
+				System.out.print(c++ + "주차 감량 몸무게 : ");
+				int d = sc.nextInt();
+			
+				sum += d;
+			
+				} 
+			System.out.println(a - sum + "kg 달성!!");
 		}
-		else System.out.println("백의 자리를 입력하세요!!");
-	
+
 	}
 
-}
+
