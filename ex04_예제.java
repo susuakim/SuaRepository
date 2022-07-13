885c1581866ff142738d4c3cb18d62d2aae9e240
김수아 <106250150+susuakim@users.noreply.github.com>
2022-07-13 09:02:38 +0900

    Add files via upload

--- ex04_예제.java
+++ ex04_예제.java
@@ -1,4 +1,4 @@
-package if문;
+package 반복문while;
 
 import java.util.Scanner;
 
@@ -8,22 +8,37 @@ public class ex04_예제 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("정수를 입력하세요:  ");
+		while(true) {
+			
+			System.out.print("첫 번째 정수를 입력하세요>> ");
+			int num1 = sc.nextInt();
+			
+			System.out.print("두 번째 정수를 입력하세요>> ");
+			int num2 = sc.nextInt();
+			
+			System.out.print("[1]더하기 [2]빼기 >> ");
+			int rlt = sc.nextInt();
+			
+			if(rlt == 1) {
+				System.out.println("더하기 연산 결과는 " + (num1+num2) + "입니다.");
+				
+			}else System.out.println("빼기 연산 결과는 " + (num1-num2) + "입니다.");
+			
+			//------------------------------		
+//			System.out.println("결과는 " + (rlt == 1 ? num1+num2 : num1-num2)); 삼항연산자 사용하면 간단
+			
+			
+			System.out.print("다시 실행하시겠습니까? (Y/N)");
+			String s = sc.next();//
+			
+			if(s.equals("N")) {
+				System.out.println("종료되었습니다.");
+				break;
+			}
+			
+		}
 		
-		int num = sc.nextInt();
 		
-		if(num%2 == 0) {
-			System.out.println(num + " 짝수입니다.");
-		} else System.out.println(num + " 홀수입니다.");
-
-	}
-	
-	//만약에 num을 2로 나우었을때 나버지가 0과 같다면 조건식 짤==ㄱ수
-	//같지 않다면 else 홀수 실행문장___sc.nextInt();
-	
-	else {
-		System.out.println();
-		System.out.println();System.out.println("ex04_예제.enclosing_method()");
 	}
 
 }
