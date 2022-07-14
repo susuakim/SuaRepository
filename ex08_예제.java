eb048b86d0dc5fa14ddbbfa39651e34902c2359e
김수아 <106250150+susuakim@users.noreply.github.com>
2022-07-14 10:15:55 +0900

    Add files via upload

--- ex08_예제.java
+++ ex08_예제.java
@@ -1,27 +1,22 @@
-package 삼항연산자;
-
-import java.util.Scanner;
+package for문;
 
 public class ex08_예제 {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("첫번째 정수 입력: ");
-		int num1 = sc.nextInt();
-		
-		System.out.print("두번째 정수 입력: ");
-		int num2 = sc.nextInt();
-		
-		int result = num1>num2 ? num1-num2 : num2-num1;
-		
-		System.out.println(result);
-		
-//		if(num1>num2) {
-//			System.out.println("두 수의 차: " + (num1-num2));
-//		}
-//		else System.out.println("두 수의 차: " + (num2-num1));
+
+
+		for(int i =1; i<=5; i++) {
+			for(int j=i-1; j<5; j++) {
+				System.out.print(" ");
+			}
+			for(int k=1; k<=i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
 	}
 
 }
+
