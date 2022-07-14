85072dbb69769527e6f3c669370d1f42cd809e6b
김수아 <106250150+susuakim@users.noreply.github.com>
2022-07-14 10:15:14 +0900

    Add files via upload

--- ex04_예제.java
+++ ex04_예제.java
@@ -1,44 +1,18 @@
-package 반복문while;
-
-import java.util.Scanner;
+package for문;
 
 public class ex04_예제 {
 
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		while(true) {
-			
-			System.out.print("첫 번째 정수를 입력하세요>> ");
-			int num1 = sc.nextInt();
-			
-			System.out.print("두 번째 정수를 입력하세요>> ");
-			int num2 = sc.nextInt();
-			
-			System.out.print("[1]더하기 [2]빼기 >> ");
-			int rlt = sc.nextInt();
-			
-			if(rlt == 1) {
-				System.out.println("더하기 연산 결과는 " + (num1+num2) + "입니다.");
-				
-			}else System.out.println("빼기 연산 결과는 " + (num1-num2) + "입니다.");
-			
-			//------------------------------		
-//			System.out.println("결과는 " + (rlt == 1 ? num1+num2 : num1-num2)); 삼항연산자 사용하면 간단
-			
-			
-			System.out.print("다시 실행하시겠습니까? (Y/N)");
-			String s = sc.next();//
-			
-			if(s.equals("N")) {
-				System.out.println("종료되었습니다.");
-				break;
-			}
-			
-		}
-		
-		
-	}
 
+		for(int i = 1; i <=50; i++) {
+			int num = i%10;
+			if(num==3 || num==6 || num==9) {
+				System.out.print("박수" + " ");
+		}else if(num==5) {
+			System.out.print("으악");
+		}else System.out.println(i);
+
+		}
+
+	}
 }
