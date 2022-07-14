10f5d6d74819850cc521370d9df524f467e8b6e5
김수아 <106250150+susuakim@users.noreply.github.com>
2022-07-14 10:14:54 +0900

    Add files via upload

--- ex02_예제.java
+++ ex02_예제.java
@@ -1,43 +1,21 @@
-package 반복문while;
-
-import java.util.Scanner;
+package for문;
 
 public class ex02_예제 {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("현재몸무게a : ");
-		int a = sc.nextInt();
-		
-		System.out.print("목표몸무게b : ");
-		int b = sc.nextInt();
-		
 		int sum = 0;
-		
-		int c = 1;
-//		while(true) {
-//			System.out.print(c++ + "주차 감량 몸무게 : ");
-//			int d = sc.nextInt();
-//		
-//			sum += d;
-//			
-//			if(sum > (a-b)) {
-//				System.out.println(a - sum + "kg 달성!!");
-//				break;
-//			} 
-			
-			while(sum < (a-b)) {
-				System.out.print(c++ + "주차 감량 몸무게 : ");
-				int d = sc.nextInt();
-			
-				sum += d;
-			
-				} 
-			System.out.println(a - sum + "kg 달성!!");
+		for(int i =1; i<=100; i++) {
+			if(i*3>100) {
+				break;
+			}
+			System.out.println(i*3 +" ");
+			if(i%3==0) {
+				sum += i;
 		}
+		System.out.println(sum);
 
 	}
-
+	}
+}
 
