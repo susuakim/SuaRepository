76e88e5c925f7711ace51bff54e5e25d776a4e1f
김수아 <106250150+susuakim@users.noreply.github.com>
2022-07-15 09:03:55 +0900

    Add files via upload

--- ex08_예제.java
+++ ex08_예제.java
@@ -1,22 +1,30 @@
-package for문;
+package 실습;
+
+import java.io.*;
+import java.util.Scanner;
 
 public class ex08_예제 {
-
-	public static void main(String[] args) {
-		
-
-
-		for(int i =1; i<=5; i++) {
-			for(int j=i-1; j<5; j++) {
-				System.out.print(" ");
-			}
-			for(int k=1; k<=i; k++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
+	public static void main(String args[]) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("숫자 입력: ");
+        int num = sc.nextInt();
+        
+        int num_c;
+        
+        if(num>=100){
+            num_c = num - (num/10)*10;
+            System.out.println("반올림숫자??" + num_c);
+            if(num_c>5){
+                num = num + (10-num_c); System.out.print("수: " + num);
+            }else num -= num_c;
+            
+            System.out.print("반올림 수: " + num);
+        }else System.out.println("100이상의 자리를 입력해주세요.");
+        
+        
+        
 
 	}
-
 }
-
