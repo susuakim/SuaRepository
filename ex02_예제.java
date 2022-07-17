46228edfa85e23dce34aea15c0491ab8ac03c64f
김수아 <106250150+susuakim@users.noreply.github.com>
2022-07-17 18:50:50 +0900

    Add files via upload

--- ex02_예제.java
+++ ex02_예제.java
@@ -1,49 +1,44 @@
-package 배열;
-
-import java.util.Scanner;
+package 이차원배열;
 
 public class ex02_예제 {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
+		//1~25까지 5행 5열 array에 초기화하기
 		
-		int[] ar = new int[5];
+		int[][] num = new int[5][5];
+		int cnt = 1;//대입할 값을 대입ㅂ
 		
-		for(int i =0; i<ar.length; i++) {
-			System.out.print((i+1) + "번째 입력>> ");
-			int num = sc.nextInt();
+		for(int i = 0; i<num.length; i++) {
+			for(int k = 0; k<num[i].length; k++) {
+				num[k][i] = cnt++;
+			}System.out.println();
 		
-			ar[i]=num;
-
-		}
-		System.out.print("입력된 점수: ");
 		
-		int sum = 0;
+		int result = 0;
 		
-		for(int j = 0; j<ar.length; j++) {
-		System.out.print(ar[j] + " ");
-		sum += ar[j];
-		} 
-		System.out.println();
+//		for(int i = 0; i < num.length; i++) {
+//			for(int k = 0; k < num[i].length; k++) {
+//				result = result + 1;
+//				num[k][i] = result;
+////				System.out.print(num[i][k] + " ");
+//			}System.out.println();
+//		}
 		
-		//최저, 최고점수, 총합, 평균 구하기------------------------
-		int max = ar[0]; 
-		int min = ar[0];
-		
-		for(int k =0; k<ar.length; k++) {
-			if(max < ar[k]) {
-				max = ar[k];
-			}
-			if(min > ar[k]) {
-				min = ar[k];
-			}
+		for(int j = 0; j < num.length; j++) {
+			for(int a = 0; a < num[j].length; a++) {
+				System.out.print(num[j][a] + " ");
+			}System.out.println();
 		}
 		
-		System.out.println("최고점수: " + max + "   " + "최저점수: " + min);
-		System.out.print("총합: " + sum + "   ");
-		System.out.println("평균: " + (float)sum/ar.length);
+		
+		}
 		
 	}
 	
 }
+
+
+	
+
+
