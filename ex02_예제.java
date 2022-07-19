a755200c1ce1f9ab4c25c008d3b76040eeaa2df7
김수아 <106250150+susuakim@users.noreply.github.com>
2022-07-19 10:18:03 +0900

    Add files via upload

--- ex02_예제.java
+++ ex02_예제.java
@@ -1,44 +1,27 @@
-package 이차원배열;
+package 메소드;
 
 public class ex02_예제 {
 
 	public static void main(String[] args) {
 		
-		//1~25까지 5행 5열 array에 초기화하기
+		int num1 = 50;
+		int num2 = 15;
+		char op = '*';
 		
-		int[][] num = new int[5][5];
-		int cnt = 1;//대입할 값을 대입ㅂ
-		
-		for(int i = 0; i<num.length; i++) {
-			for(int k = 0; k<num[i].length; k++) {
-				num[k][i] = cnt++;
-			}System.out.println();
-		
-		
-		int result = 0;
-		
-//		for(int i = 0; i < num.length; i++) {
-//			for(int k = 0; k < num[i].length; k++) {
-//				result = result + 1;
-//				num[k][i] = result;
-////				System.out.print(num[i][k] + " ");
-//			}System.out.println();
-//		}
-		
-		for(int j = 0; j < num.length; j++) {
-			for(int a = 0; a < num[j].length; a++) {
-				System.out.print(num[j][a] + " ");
-			}System.out.println();
-		}
-		
-		
-		}
-		
+		System.out.println(cal(num1, num2, op));
+
 	}
 	
+	public static int cal(int num1, int num2, char op) {
+		
+		switch(op) {
+		case'+': return num1+num2;
+		case'-': return num1-num2;
+		case'*': return num1*num2;
+		case'/': return num1/num2;
+		}
+		
+		return 0;
+	}
+
 }
-
-
-	
-
-
