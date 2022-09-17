35da4dbab4c668d215fc8a478865384c6c0280a2
김수아 <106250150+susuakim@users.noreply.github.com>
2022-09-17 15:11:59 +0900

    Rename ex03_ifelse문.java to 언어/Java/ifelse문.java

---
+++ 언어/Java/ifelse문.java
@@ -0,0 +1,16 @@
+package if문;
+
+public class ex03_ifelse문 {
+
+	public static void main(String[] args) {
+//		조건식이 true라면 if문의 실행문장 실행
+//		false라면 else문의 실행문장을 실행
+//	else문에는 조건식이 없음!!
+	
+		
+		System.out.println("if문의 실행문장");
+		System.out.println("else문의 실행문장");
+		 
+	}
+
+}
