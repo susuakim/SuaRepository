46b9b6467128b3e5beca799d6517459604a7c17e
김수아 <106250150+susuakim@users.noreply.github.com>
2022-09-17 14:59:16 +0900

    Rename ex01_이차원배열.java to 언어/Java/이차원 배열.java

---
+++ 언어/Java/이차원 배열.java
@@ -0,0 +1,22 @@
+package 이차원배열;
+
+public class ex01_이차원배열 {
+
+	public static void main(String[] args) {
+		//빅데이터반
+		int[] java = new int[25];
+		int[] db = new int[25];
+		
+		//이차원 배열 선언 및 생성하는 방법
+		int[][] bigdata = new int[2][25];
+		
+		//첫번 [] 바깥U배열 과목수, 두번째 [] 안U배열 사람수
+		//인공지능반
+		
+		System.out.println();
+		
+		//비열명[][] = 값 호출
+		//배열명[][] = 값 변경 대입
+	}
+
+}
