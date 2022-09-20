8b083ef7ac7514b1f901eb8ffe66164044ac6c3b
김수아 <106250150+susuakim@users.noreply.github.com>
2022-09-20 15:49:33 +0900

    Add files via upload

---
+++ 언어/Java/동물/Dog.java
@@ -0,0 +1,16 @@
+package 悼拱;
+
+public class Dog extends Animal {
+	
+	public void cry() {
+		System.out.println("港港");
+	}
+	
+	public void eat() {
+		System.out.println("港港捞 橙橙");
+	}
+	
+	public void sleep() {
+		System.out.println("港港捞 儡促");
+	}
+}
